f373015c9a5e72a567769341273cca00b843e7db
Raynaldo Winson <winsonamadeus.kusuma@gmail.com>
2025-09-22 01:24:19 +0700

    moved Player class into its own class

---
+++ Player.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <format>
+
+#include "Stock.h"
+#include "Card.h"
+
+class Player {
+	public:
+		int cash;
+		std::vector<Stock> portfolio;
+		std::vector<Card> cards;
+		std::string name;
+
+        Player();
+		Player(int cash, std::string name);
+		Player(std::string name);
+
+		void set_name(std::string s);
+		void show_menu(const std::vector<Stock>& chart);
+		void display_cards();
+};
