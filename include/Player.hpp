6e80cb4bb4474a5cb18e94f9fe64b990396d85e0
Raynaldo Winson <winsonamadeus.kusuma@gmail.com>
2025-09-30 19:20:09 +0700

    moved all Player class attributes as private and reindented the file

--- include/Player.hpp
+++ include/Player.hpp
@@ -8,17 +8,18 @@
 #include "Card.hpp"
 
 class Player {
-	public:
-	int cash;
-	std::vector<Stock> portfolio;
-	std::vector<Card> cards;
-	std::string name;
+  private:
+    int cash;
+	  std::vector<Stock> portfolio;
+	  std::vector<Card> cards;
+	  std::string name;
 
-	Player();
-	Player(int cash, std::string name);
-	Player(std::string name);
+  public:
+    Player();
+    Player(int cash, std::string name);
+    Player(std::string name);
 
-	void set_name(std::string s);
-	void show_menu(const std::vector<Stock>& chart);
-	void display_cards();
+    void set_name(std::string s);
+    void show_menu(const std::vector<Stock>& chart);
+    void display_cards();
 };
